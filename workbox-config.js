72107c9d95d8def20ab78f5cc93179a94f56ddfd
Jeremy Karlsson <karlsson@jeremy.se>
2020-04-04 17:39:01 +0200

    Create workbox config

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "globDirectory": ".",
+  "globPatterns": [
+    "*.{html,css,js,json}",
+    "data/*.json",
+    "created-page/*.html",
+    "components/*.{html,css,js}",
+    "img/*.{png,svg,ico}"
+  ],
+  "swDest": "sw.js"
+};
